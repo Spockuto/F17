6f4010c235fcec8927ba73842c1dbae42a07f7b6
Anirudh Sundar <anirudhsundar@hotmail.com>
2017-07-29 15:07:33 +0000

    Added Dojo

--- javascripts/openingScreen.js
+++ javascripts/openingScreen.js
@@ -1,14 +1,9 @@
-// Basic version of the opening screen
-// Doorknob still looks weird though
+document.addEventListener('click', enter);
 
-document.addEventListener('click', () => {
+function enter() {
 	openDoors();
 	setTimeout(exit, 500);
-});
-
-// window.addEventListener('keydown', () => {
-// 	reset();
-// });
+}
 
 function openDoors() {
 	var overlays = document.querySelectorAll('.door-overlay');
@@ -25,18 +20,7 @@ function openDoors() {
 function exit() {
 	var translateZ = 20;
 	var perspective = 20;
-	var zoomFactor = perspective / (perspective - translateZ);
-
-	// Percentage Distance of door from top of castle
-	var doorCenter = 57.21 / 100;
-	var castleHeight = document.querySelector("#castle-container").offsetHeight;
-	var screenHeight = window.innerHeight;
-
-	// dist = Dist between originY pt and center of door
-	var dist = ( (doorCenter - 1)*castleHeight + screenHeight/2 ) / (zoomFactor - 1);
-
-	// height of pt from the top (+1 is a correction to account for translation)
-	var originY = 100 * ( dist + (doorCenter - 1)*castleHeight + screenHeight ) / screenHeight  +  2;
+	var originY = originYCalc(translateZ, perspective);
 
 	// Chrome for Android bug workaround
 	if (/.*Android.*Chrome.*/i.test(navigator.userAgent))
@@ -44,6 +28,7 @@ function exit() {
 	else
 		var waitTime = 2000;
 
+	// Style Changes
 	var body = document.body;
 	var mainContainer = document.querySelector('div#opening-screen-container');
 	var castleContainer = document.querySelector('div#castle-container');
@@ -56,6 +41,22 @@ function exit() {
 	castleFadeOut(mainContainer, loader, waitTime);
 }
 
+function originYCalc(translateZ, perspective) {
+	var zoomFactor = perspective / (perspective - translateZ);
+
+	// Percentage Distance of door from top of castle
+	var doorCenter = 57.21 / 100;
+	var castleHeight = document.querySelector("#castle-container").offsetHeight;
+	var screenHeight = window.innerHeight;
+
+	// dist = Dist between originY pt and center of door
+	var dist = ( (doorCenter - 1)*castleHeight + screenHeight/2 ) / (zoomFactor - 1);
+
+	// height of pt from the top (+1 is a correction to account for translation)
+	var originY = 100 * ( dist + (doorCenter - 1)*castleHeight + screenHeight ) / screenHeight  +  2;
+	return originY;
+}
+
 function castleFadeOut(element, loader, time) {
 	element.style.opacity = 0;
 	setTimeout(function() { loader.style.opacity = 1; }, 2000);
@@ -63,5 +64,8 @@ function castleFadeOut(element, loader, time) {
 		element.style.display = "none";
 		loader.style.opacity = 0;
 		setTimeout(function() { loader.style.display = "none"; }, 1000);
+		document.removeEventListener('click', enter);
+
+		// Dojo Fade in
 	}, time);
 }
