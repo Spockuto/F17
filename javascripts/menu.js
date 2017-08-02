b5b647989128a561592bb88bb81c59103d48da9d
Anirudh Sundar <anirudhsundar@hotmail.com>
2017-08-02 20:34:03 +0000

    Removed zooming in dojo

--- javascripts/menu.js
+++ javascripts/menu.js
@@ -59,25 +59,25 @@ function ninjaChange() {
 }
 
 
-var menuContainer = document.querySelector("div#menu-container");
-menuContainer.addEventListener('mousedown', function(event) {
-	if (event.button == 0) {
-		dojoZoom('0em');
-		svgFade(0);
-	} else {
-		dojoZoom('-12em');
-		svgFade(1);
-	}
-});
+// var menuContainer = document.querySelector("div#menu-container");
+// menuContainer.addEventListener('mousedown', function(event) {
+// 	if (event.button == 0) {
+// 		dojoZoom('0em');
+// 		svgFade(0);
+// 	} else {
+// 		dojoZoom('-12em');
+// 		svgFade(1);
+// 	}
+// });
 
-function svgFade(opacity) {
-	frontContainer = document.querySelector("div#front-dojo-container");
-	frontContainer.style.opacity = opacity;
-}
+// function svgFade(opacity) {
+// 	frontContainer = document.querySelector("div#front-dojo-container");
+// 	frontContainer.style.opacity = opacity;
+// }
 
-function dojoZoom(zoomVal) {
-	menuContainer.style.transform = "translateZ(" + zoomVal + ")";
-}
+// function dojoZoom(zoomVal) {
+// 	menuContainer.style.transform = "translateZ(" + zoomVal + ")";
+// }
 
 function redirect() {
 	location.reload();
